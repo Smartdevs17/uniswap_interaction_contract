493302ab6de6ba9a1d2b713b3c56bdfadb2cc8c2
smartdev <smartdevs17@gmail.com>
2024-09-21 00:53:58 +0100

    update tests

--- README.md
+++ README.md
@@ -1,15 +1,45 @@
-# Sample Hardhat Project
+# Uniswap Interface and Testing
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+This repository contains the interface for interacting with the Uniswap V2 Router and the tests that were conducted on it. The Uniswap V2 Router is a key component of the Uniswap decentralized exchange protocol, allowing users to swap tokens, add liquidity, and remove liquidity.
 
-Try running some of the following tasks:
+## Interface Methods
 
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat ignition deploy ./ignition/modules/Lock.ts
-```
-# uniswap_interaction_contract
-# uniswap_interaction_contract
+### `swapExactTokensForTokens`
+This method allows users to swap an exact amount of input tokens for as many output tokens as possible, along a specified path.
+
+### `swapTokensForExactTokens`
+This method allows users to swap tokens such that the exact amount of output tokens is received, along a specified path.
+
+### `swapTokensForExactETH`
+This method allows users to swap tokens for an exact amount of ETH, along a specified path.
+
+### `addLiquidity`
+This method allows users to add liquidity to a pair of tokens.
+
+### `removeLiquidity`
+This method allows users to remove liquidity from a pair of tokens.
+
+### `swapExactETHForTokens`
+This method allows users to swap an exact amount of ETH for as many output tokens as possible, along a specified path.
+
+## Testing
+
+The following tests were conducted to ensure the functionality of the Uniswap V2 Router interface:
+
+1. **Swap Exact Tokens For Tokens Test**
+   - Verified that the correct amount of output tokens is received when swapping an exact amount of input tokens.
+
+2. **Swap Tokens For Exact Tokens Test**
+   - Verified that the correct amount of input tokens is used when swapping for an exact amount of output tokens.
+
+3. **Swap Tokens For Exact ETH Test**
+   - Verified that the correct amount of input tokens is used when swapping for an exact amount of ETH.
+
+4. **Add Liquidity Test**
+   - Verified that liquidity is correctly added to a pair of tokens.
+
+5. **Remove Liquidity Test**
+   - Verified that liquidity is correctly removed from a pair of tokens.
+
+6. **Swap Exact ETH For Tokens Test**
+   - Verified that the correct amount of output tokens is received when swapping an exact amount of ETH.
--- contracts/UseSwap.sol
+++ contracts/UseSwap.sol
@@ -166,42 +166,6 @@ contract UseSwap {
         swapCount += 1;
     }
 
-    // function addLiquidityETH(
-    //     address token,
-    //     uint amountTokenDesired,
-    //     uint amountTokenMin,
-    //     uint amountETHMin,
-    //     address to,
-    //     uint deadline
-    // ) external payable {    
-
-    //     // Transfer tokens from the sender to the contract
-    //     // IERC20(token).transferFrom(msg.sender, address(this), amountTokenDesired);
-
-    //     IERC20(token).approve(uniswapRouter, amountTokenDesired);
-    //     uint balance = IERC20(token).balanceOf(msg.sender);
-    //     console.log("balance", balance);
-    //     console.log("amountTokenDesired", amountTokenDesired);
-    //     console.log("amountETHMin", amountETHMin);
-    //     console.log("msg.value", msg.value);
-
-    //     // Approve the Uniswap router to spend the tokens
-    //     require(IERC20(token).approve(uniswapRouter, amountTokenDesired), "approve token failed.");
-
-    //     // Add liquidity using the Uniswap router
-    //     IUniswapV2Router(uniswapRouter).addLiquidityETH{value: msg.value}(
-    //         token,
-    //         amountTokenDesired,
-    //         amountTokenMin,
-    //         amountETHMin,
-    //         to,
-    //         deadline
-    //     );
-
-    //     // Increment the swap count
-    //     swapCount += 1;
-    // }
-
     function addLiquidityETH(
     address token,
     uint amountTokenDesired,
@@ -213,7 +177,7 @@ contract UseSwap {
     // Check token balance of msg.sender
     uint balance = IERC20(token).balanceOf(msg.sender);
     require(balance >= amountTokenDesired, "Insufficient token balance");
-    IERC20(token).transferFrom(msg.sender, address(this), amountTokenDesired);
+    IERC20(token).transferFrom(msg.sender, uniswapRouter, amountTokenDesired);
 
     console.log("balance", balance);
     console.log("amountTokenDesired", amountTokenDesired);
@@ -226,7 +190,7 @@ contract UseSwap {
     // Add liquidity using the Uniswap router
     IUniswapV2Router(uniswapRouter).addLiquidityETH{value: msg.value}(
         token,
-        amountTokenDesired,
+        1,
         amountTokenMin,
         amountETHMin,
         to,
@@ -236,92 +200,6 @@ contract UseSwap {
     // Increment the swap count
     swapCount += 1;
 }
-
-        
-
-    // // Function to handle exact ETH swaps for tokens
-    // function handleExactETHSwapForTokens(
-    //     uint amountOut,
-    //     uint amountInMax,
-    //     address[] calldata path,
-    //     address to,
-    //     uint deadline
-    // ) external payable {
-
-    //     // Approve the Uniswap router to spend the ETH
-    //     require(msg.value > 0, "ETH amount must be greater than 0.");
-        
-    //     // Swap ETH for tokens using the Uniswap router
-    //     IUniswapV2Router(uniswapRouter).swapExactTokensForETH(
-    //         amountOut,
-    //         amountInMax,
-    //         path,
-    //         to,
-    //         deadline
-    //     );
-
-    //     // Increment the swap count
-    //     swapCount += 1;
-    // }
-
-    // // Function to add liquidity
-
-
-
-
-
-   // Function to handle exact token swaps for ETH
-    // function handleExactSwapForETH(
-    //     uint amountIn,
-    //     uint amountOutMin,
-    //     address[] calldata path,
-    //     address to,
-    //     uint deadline
-    // ) external {        
-
-    //     // Transfer tokens from the sender to the contract
-    //     IERC20(path[0]).transferFrom(msg.sender, address(this), amountIn);
-
-    //     // Approve the Uniswap router to spend the tokens
-    //     require(IERC20(path[0]).approve(uniswapRouter, amountIn), "approve failed.");
-        
-    //     // Swap tokens for ETH using the Uniswap router
-    //     IUniswapV2Router(uniswapRouter).swapExactTokensForETH(
-    //         amountIn,
-    //         amountOutMin,
-    //         path,
-    //         to,
-    //         deadline
-    //     );
-
-    //     // Increment the swap count
-    //     swapCount += 1;
-    // }
-
-
- 
-    // // Function to handle ETH swaps for tokens
-    // function handleETHSwapForTokens(
-    //     uint amountOutMin,
-    //     address[] calldata path,
-    //     address to,
-    //     uint deadline
-    // ) external payable {
-
-    //     // Approve the Uniswap router to spend the ETH
-    //     require(msg.value > 0, "ETH amount must be greater than 0.");
-        
-    //     // Swap ETH for tokens using the Uniswap router
-    //     IUniswapV2Router(uniswapRouter).swapExactETHForTokens(
-    //         amountOutMin,
-    //         path,
-    //         to,
-    //         deadline
-    //     );
-
-    //     // Increment the swap count
-    //     swapCount += 1;
-    // }
 
     receive() external payable {}
 
